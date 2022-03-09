bfad9ee9075e9f751c667d6313bd7f390bbc8c14
Steve Cameron <45084952+SCameron99@users.noreply.github.com>
2022-03-09 07:22:07 -0500

    Gauss Jordan working properly

--- HiddenFortess.py
+++ HiddenFortess.py
@@ -37,35 +37,52 @@ for i in range(size):
         equations[input_index].append(int(i))
         input_index += 1
 
+
+
 #My first attempt at Gauss Jordan
 current_line = 0
 ratio = 0
 divider = 1
 for i in range(size*size):
+    #Pushing line with pivot = 0 to the end
     while equations[current_line][current_line] == 0:
-        equations[current_line] += [equations[current_line].pop(current_line)]
+        equations += [equations.pop(current_line)] 
 
     #Reducing pivot to 1
     divider = equations[current_line][current_line]
-    print(current_line)
-    print(divider)
     element_checked = 0
     for j in equations[current_line]:
         equations[current_line][element_checked] = round(equations[current_line][element_checked]/divider, 3)
         element_checked += 1
-        
+    
+    #print("BEFORE") #Used for debugging
+    #print(current_line)
 
     #Putting zeros on the current column
+
+    #for i in equations: #Debugging
+        #print(i[current_line])
+
     line_checked = 0
     for j in equations:
-        if j[current_line] != 0 and line_checked != current_line:
-            modifier = -1*j[current_line]
-            for k in range(size*size + 1):
-                j[k] = j[k] + modifier*equations[current_line][k] 
-        line_checked += 1
+        if float(equations[line_checked][current_line]) != 0 and line_checked != current_line:
+            element_checked = 0
+            modifier = -1*equations[line_checked][current_line]
 
+            #print("HI " + str(line_checked)) #Debugging
+            
+            for k in range(size*size + 1):
+                equations[line_checked][element_checked] = round(equations[line_checked][element_checked] + modifier*equations[current_line][element_checked], 3)
+                element_checked += 1
+        line_checked += 1
+    
+    #for i in equations: #Debugging
+        #print(i[current_line])
+        
     current_line += 1
+    
     # Write an answer using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-    print(equations)
+print(equations)  
+
