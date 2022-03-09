b8c7b4a335306bba726c203f619b88f7c89ceb10
Steve Cameron <45084952+SCameron99@users.noreply.github.com>
2022-03-09 07:32:49 -0500

    Works for 3x3 grids

--- HiddenFortess.py
+++ HiddenFortess.py
@@ -78,11 +78,22 @@ for i in range(size*size):
     
     #for i in equations: #Debugging
         #print(i[current_line])
-        
+
     current_line += 1
     
     # Write an answer using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print(equations)  
+
+#formatting the output
+line_checked = 0
+for i in range(size):
+    answer = ""
+    for j in range(size):
+        if int(equations[line_checked][-1]) == 1:
+            answer += "O"
+        else:
+            answer += "."
+        line_checked += 1
+    print(answer)
 
