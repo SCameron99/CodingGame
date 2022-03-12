5e48c05a1c733413116aa9e64a3cf7fa711ff885
Steve Cameron <45084952+SCameron99@users.noreply.github.com>
2022-03-12 08:33:56 -0500

    100%

--- CourseChevaux.py
+++ CourseChevaux.py
@@ -1,24 +1,18 @@
 import sys
 import math
+import numpy as np
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
 pwr = []
+
 n = int(input())
 for i in range(n):
     pi = int(input())
     pwr.append(pi)
 
-d_min = 999999999
-for i in range(len(pwr)):
-    for j in range(len(pwr)):
-        if i != j :
-            if abs(pwr[i] - pwr[j]) < d_min:
-                d_min = abs(pwr[i] - pwr[j])
-                answer = d_min
-
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print(answer)
+print(np.min(np.diff(np.sort(np.array(pwr)))))
