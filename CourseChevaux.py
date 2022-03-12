9157d51c632313cd3a6e2929faadf31d5fee2b2e
Steve Cameron <45084952+SCameron99@users.noreply.github.com>
2022-03-12 17:34:25 -0500

    Update CourseChevaux.py

--- CourseChevaux.py
+++ CourseChevaux.py
@@ -5,12 +5,9 @@ import numpy as np
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-pwr = []
-
-n = int(input())
+n, pwr = int(input()), []
 for i in range(n):
-    pi = int(input())
-    pwr.append(pi)
+    pwr.append(int(input()))
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
