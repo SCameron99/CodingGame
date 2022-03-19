a86d14482f82317a64abf779c93c46922d882da9
Steve Cameron <45084952+SCameron99@users.noreply.github.com>
2022-03-19 08:44:24 -0400

    Create BrainFork.py

---
+++ BrainFork.py
@@ -0,0 +1,26 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+char_values = {" ": 0, "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13,
+               "Z" : -1, "Y": -2, "X": -3, "W": -4, "V": -5, "U": -6, "T": -7, "S": -8, "R": -9, "Q": -10, "P": -11, "O": -12, "N": -13}
+
+stones_values = {"1": " "}
+
+magic_phrase = input()
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+solution = ""
+for i in magic_phrase:
+    x = char_values[i]
+    for j in range(abs(x)):
+        if x > 0:
+            solution += "+"
+        elif x < 0:
+            solution += "-"
+    solution += ".>"
+print(solution)
