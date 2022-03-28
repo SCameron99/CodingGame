542dd24b3a1f9058d0abf082ac0fc725caa80951
Steve Cameron <45084952+SCameron99@users.noreply.github.com>
2022-03-28 07:51:55 -0400

    Create ThereIsNoSpoon.py

---
+++ ThereIsNoSpoon.py
@@ -0,0 +1,44 @@
+import sys
+import math
+
+# Don't let the machines win. You are humanity's last hope...
+
+width = int(input())  # the number of cells on the X axis
+height = int(input())  # the number of cells on the Y axis
+lines = []
+for i in range(height):
+    line = input()  # width characters, each either 0 or .
+    lines.append(list(line))
+for y in range(height):
+    for x in range(width):
+       if lines[y][x]==".":
+           continue
+       rx = ry = bx = by = -1
+       try:
+           for tx in range(x+1,width):
+               if(lines[y][tx]=='0'):
+                   rx = tx
+                   ry = y
+                   break
+       except Exception:
+           pass
+       try:
+           for ty in range(y+1, height):
+               if(lines[ty][x]=='0'):
+                   bx = x
+                   by =ty
+                   break
+       except Exception:
+           pass
+       print("{0} {1} {2} {3} {4} {5}".format(x, y, rx, ry, bx, by))
+    
+
+
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+# Three coordinates: a node, its right neighbor, its bottom neighbor
+#print("0 0 1 0 0 1")
+#print(output)
