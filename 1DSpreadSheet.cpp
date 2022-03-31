43ea766d6de90fc42abab04bc0d74ae0996e5792
Steve Cameron <45084952+SCameron99@users.noreply.github.com>
2022-03-31 18:04:51 -0400

    Update 1DSpreadSheet.cpp

--- 1DSpreadSheet.cpp
+++ 1DSpreadSheet.cpp
@@ -21,6 +21,7 @@ int main()
     string operations [n] {};
     string args_1 [n] {};
     string args_2 [n] {};
+    string output_array [n] {};
     for (int i = 0; i < n; i++) {
         string operation;
         string arg_1;
@@ -35,25 +36,34 @@ int main()
     for (int i = 0; i < n; i++) {
         int arg_1_int {};
         int arg_2_int {};
-        if (args_1[i].front() == '$') {
-            args_1[i].erase(0,1);
-            arg_1_int = stoi(args_1[i]);
-            args_1[i] = args_1[arg_1_int];
-
+        string cur_arg_1 {args_1[i]};
+        string cur_arg_2 {args_2[i]};
+        while (cur_arg_1.front() == '$') {
+            int arg_1_idx {};
+            string arg_1_idx_str {};
+            arg_1_idx_str = cur_arg_1.substr(1);
+            arg_1_idx = stoi(arg_1_idx_str);
+            //arg_1_int = stoi(args_1[i]);
+            cur_arg_1 = args_1[arg_1_idx];
         }
-        if (args_2[i].front() == '$') {
-            args_2[i].erase(0,1);
-            arg_2_int = stoi(args_2[i]);
-            args_2[i] = args_2[arg_2_int];
+        while (cur_arg_2.front() == '$') {
+            int arg_2_idx {};
+            string arg_2_idx_str {};
+            arg_2_idx_str = cur_arg_2.substr(1);
+            arg_2_idx = stoi(arg_2_idx_str);
+            //arg_2_int = stoi(args_2[i]);
+            cur_arg_2 = args_2[arg_2_idx];
         }
-        arg_1_int = stoi(args_1[i]);
-        if (args_2[i] != "_") {
-            arg_2_int = stoi(args_2[i]);
+        arg_1_int = stoi(cur_arg_1);
+        if (cur_arg_2 != "_") {
+            arg_2_int = stoi(cur_arg_2);
         }
+        //cout << operations[i] << endl << cur_arg_1 << endl << cur_arg_2 << endl;
+        cout << args_1[i] << endl << args_2[i] << endl;
         string output {};
         int output_int {};
         if (operations[i] == value) {
-            cout << args_1[i] << endl;
+            cout << cur_arg_1 << endl;
         }
         if (operations[i] == add) {
             output_int = arg_1_int + arg_2_int;
