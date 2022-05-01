79b2701bf4d2215663ee7f4c4ca301f358a54dd0
Steve Cameron <45084952+SCameron99@users.noreply.github.com>
2022-05-01 07:52:25 -0400

    Update SpringChallenge2022.py

--- SpringChallenge2022.py
+++ SpringChallenge2022.py
@@ -33,13 +33,13 @@ base_x, base_y = [int(i) for i in input().split()]
 heroes_per_player = int(input())  # Always 3
 
 if base_x == 0:
-    standby1 = "MOVE 12500 5000"
+    standby1 = "MOVE 15000 6000"
     standby2 = "MOVE 5500 1500"
     standby3 = "MOVE 1500 5500"
     e_base_x = "17630"
     e_base_y = "9000"
 else:
-    standby1 = "MOVE 13000 5000"
+    standby1 = "MOVE 2500 2500"
     standby2 = "MOVE 11500 8000" 
     standby3 = "MOVE 15500 3500"
     e_base_x = "0"
@@ -63,7 +63,7 @@ while True:
     entity_count = int(input())  # Amount of heros and monsters you can see
 
     
-
+    e_base_pop = []
     monster_list = []
     my_heroes = []
     cpu_heroes = []
@@ -87,6 +87,9 @@ while True:
         elif _type == 2:
             cpu_heroes.append(entity)
 
+        if check_distance(e_base_x, x, e_base_y, y) < 6000:
+            e_base_pop.append(entity)
+
     my_dangers = []
     for i in monster_list:
         if i.nearBase == 1 and i.threatFor == 1:
@@ -97,19 +100,27 @@ while True:
         if i.threatFor == 1:
             my_threats.append(i)
 
-    no_ignore = []
+    no_ignore = [] #Monsters not heading toward oppopent
     for i in monster_list:
         if i.threatFor != 2:
             no_ignore.append(i)
 
+    cpu_threat = []
+    for i in monster_list:
+        if i.threatFor == 2:
+            cpu_threat.append(i)
+
     if len(my_dangers) == 0:
         if len(my_threats) == 0:
             if len(monster_list) > 0:
-                hero1 = "MOVE " + str(monster_list[0].x) + " " + str(monster_list[0].y)
+                target1 = find_closest(my_heroes[0], monster_list)
+                hero1 = "MOVE " + str(target1.x) + " " + str(target1.y)
+                target3 = find_closest(my_heroes[0], monster_list)
+                hero3 = "MOVE " + str(target3.x) + " " + str(target3.y)
             else:
                 hero1 = standby1
             #hero2 = standby2
-            hero3 = standby3
+                hero3 = standby3
 
         elif len(my_threats) == 1:
             hero1 = "MOVE " + str(my_threats[0].x) + " " + str(my_threats[0].y)
@@ -122,7 +133,7 @@ while True:
             hero3 = "MOVE " + str(my_threats[1].x) + " " + str(my_threats[1].y)
 
         elif len(my_threats) > 2:
-            if player_mana > 30:
+            if player_mana > 30 and turn > 100:
                 if check_distance(my_heroes[0].x, my_threats[0].x, my_heroes[0].y, my_threats[0].y) < 2000:
                     hero1 = "SPELL CONTROL " + str(my_threats[0].id) + " " + e_base_x + " " + e_base_y
                 else:
@@ -152,16 +163,14 @@ while True:
             #hero2 = "MOVE " + str(my_dangers[0].x) + " " + str(my_dangers[0].y) 
             hero3 = "MOVE " + str(my_dangers[0].x) + " " + str(my_dangers[0].y) 
 
-    if player_mana > 40 and my_heroes[0].shieldLife == 0 and turn > 114:
-        hero1 = "SPELL SHIELD 0" + str(my_heroes[0].id)
+    
 
     #if player_mana > 130 and my_heroes[1].shieldLife == 0:
         #hero1 = "SPELL SHIELD " + str(my_heroes[1].id)
 
-    if player_mana > 40 and my_heroes[2].shieldLife == 0 and turn > 114:
-        hero3 = "SPELL SHIELD " + str(my_heroes[2].id)
+    
 
-    # Hero2 Algo (ID == 1)
+    # Hero2 Algo (ID == 1) Front Line
     if turn < 15:
         hero2 = standby1
     elif turn < 100:
@@ -169,26 +178,101 @@ while True:
         hero2 = "MOVE " + str(target.x) + " " + str(target.y)
     else:
         if my_heroes[1].shieldLife == 0:
-            hero2 = "SPELL SHIELD 1"
+            hero2 = "SPELL SHIELD " + str(my_heroes[1].id)
         else:
-            for i in my_heroes:
-                if i.id == 1:
-                    if (base_x == 0 and (i.x < 11000 or i.y < 3000)) or (base_x == 17630 and (i.x > 5500 or i.y > 5500)):
-                        hero2 = standby1
-                    else:
-                        target = find_closest(my_heroes[1], monster_list)
-                        if target.threatFor == 2 and player_mana > 100:
-                            if check_distance(my_heroes[1].x, target.x, my_heroes[1].y, target.y) < 2100:
-                                hero2 = "SPELL SHIELD " + str(target.id)
-                            else:
-                                hero2 = "MOVE " + str(target.x) + " " + str(target.y)
-                        elif player_mana > 100:
-                            if check_distance(my_heroes[1].x, target.x, my_heroes[1].y, target.y) < 2100:
-                                hero2 = "SPELL CONTROL " + str(target.id) + " " + e_base_x + " " + e_base_y
-                            else:
-                                hero2 = "MOVE " + str(target.x) + " " + str(target.y)
-                        else:
+            if (base_x == 0 and (my_heroes[1].x < 14130 or my_heroes[1].y < 5500)) or (base_x == 17630 and (my_heroes[1].x > 2500 or my_heroes[1].y > 2500)):
+                hero2 = standby1
+            else:
+                if len(cpu_threat) > 0:
+                    target = find_closest(my_heroes[1], cpu_threat)
+                    if target.shieldLife == 0:
+                        if check_distance(my_heroes[1].x, target.x, my_heroes[1].y, target.y) > 2000:
                             hero2 = "MOVE " + str(target.x) + " " + str(target.y)
+                        else:
+                            hero2 = "SPELL SHIELD " + str(target.id)
+                    else:
+                        target = find_closest(my_heroes[1], no_ignore)
+                        if check_distance(my_heroes[1].x, target.x, my_heroes[1].y, target.y) > 2000:
+                            hero2 = "MOVE " + str(target.x) + " " + str(target.y)
+                        else:
+                            hero2 = "SPELL CONTROL " + str(target.id) + " " + e_base_x + " " + e_base_y
+                else:
+                    target = find_closest(my_heroes[1], no_ignore)
+                    if check_distance(my_heroes[1].x, target.x, my_heroes[1].y, target.y) > 2000:
+                        hero2 = "MOVE " + str(target.x) + " " + str(target.y)
+                    else:
+                        hero2 = "SPELL CONTROL " + str(target.id) + " " + e_base_x + " " + e_base_y
+                        
+
+    # Hero1 Algo (ID == 0) Base Guard
+    if len(my_dangers) != 0:
+        dist = 99999
+        target = my_dangers[0]
+        if base_x == 0:
+            for i in my_dangers:
+                d = check_distance(0, i.x, 0, i.y)
+                if d < dist:
+                    dist = d
+                    target = i
+            if target.shieldLife > 0 or check_distance(my_heroes[0].x, target.x, my_heroes[0].y, target.y) > 1200:
+                hero1 = "MOVE " + str(target.x) + " " + str(target.y)
+            else:
+                if player_mana > 20:
+                    hero1 = "SPELL WIND " + e_base_x + " " + e_base_y
+                else:
+                    hero1 = "MOVE " + str(target.x) + " " + str(target.y)
+        else:
+            for i in my_dangers:
+                d = check_distance(17630, i.x, 9000, i.y)
+                if d < dist:
+                    dist = d
+                    target = i
+            if target.shieldLife > 0 or check_distance(my_heroes[0].x, target.x, my_heroes[0].y, target.y) > 1200:
+                hero1 = "MOVE " + str(target.x) + " " + str(target.y)
+            else:
+                if player_mana > 20:
+                    hero1 = "SPELL WIND " + e_base_x + " " + e_base_y
+                else:
+                    hero1 = "MOVE " + str(target.x) + " " + str(target.y)
+    
+    # Hero3 Algo (ID == 2)
+    if len(my_dangers) != 0:
+        dist = 99999
+        target = my_dangers[0]
+        if base_x == 0:
+            for i in my_dangers:
+                d = check_distance(0, i.x, 0, i.y)
+                if d < dist:
+                    dist = d
+                    target = i
+            if target.shieldLife > 0 or check_distance(my_heroes[2].x, target.x, my_heroes[2].y, target.y) > 1200:
+                hero3 = "MOVE " + str(target.x) + " " + str(target.y)
+            else:
+                if player_mana > 20:
+                    hero3 = "SPELL WIND " + e_base_x + " " + e_base_y
+                else:
+                    hero3 = "MOVE " + str(target.x) + " " + str(target.y)
+        else:
+            for i in my_dangers:
+                d = check_distance(17630, i.x, 9000, i.y)
+                if d < dist:
+                    dist = d
+                    target = i
+            if target.shieldLife > 0 or check_distance(my_heroes[2].x, target.x, my_heroes[2].y, target.y) > 1200:
+                hero3 = "MOVE " + str(target.x) + " " + str(target.y)
+            else:
+                if player_mana > 20:
+                    hero3 = "SPELL WIND " + e_base_x + " " + e_base_y
+                else:
+                    hero3 = "MOVE " + str(target.x) + " " + str(target.y)
+
+    if player_mana > 40 and my_heroes[0].shieldLife == 0 and turn > 100:
+        hero1 = "SPELL SHIELD 0" + str(my_heroes[0].id)
+        
+    if player_mana > 40 and my_heroes[2].shieldLife == 0 and turn > 100:
+        hero3 = "SPELL SHIELD " + str(my_heroes[2].id)
+
+
 
     
     for i in range(3):
